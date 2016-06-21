d470b52b8c853eefb0d6aba7eaf0ef5c270610e4
Christopher Langton <chrislangton84@gmail.com>
2016-06-21 14:03:05 +1000

    Create Functional.php

---
+++ src/Functional.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types = 1);
+namespace Pattern;
+
+
+/**
+ * Class Functional
+ */
+abstract class Functional {
+  /**
+   * @param array $items
+   * @param $func
+   * @return array
+   * @throws Functional\Exception
+   */
+  public static function map(array $items, $func): array {
+    if (!is_callable($func)) {
+      throw new Functional\Exception('Functional::map argument 2 ($func) must be of type Closure', 0, E_USER_ERROR,
+        __FILE__, __LINE__);
+    }
+    $results = [];
+    foreach ($items as $key => $item) {
+      $results[] = $func($item, $key);
+    }
+
+    return $results;
+  }
+
+  /**
+   * @param array $items
+   * @param $func
+   * @throws Functional\Exception
+   */
+  public static function each(array $items, $func): void {
+    if (!is_callable($func)) {
+      throw new Functional\Exception('Functional::each argument 2 ($func) must be of type Closure', 0, E_USER_ERROR,
+        __FILE__, __LINE__);
+    }
+    foreach ($items as $key => $item) {
+      $func($item, $key);
+    }
+  }
+}
+
