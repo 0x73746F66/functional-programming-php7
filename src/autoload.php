28e9ddc09659056aabe960b4f5cf08c65e38a7b4
chrisdlangton <2110 Hawa11>
2016-07-06 11:48:35 +1000

    Added test runner and fixed autoloader

--- src/autoload.php
+++ src/autoload.php
@@ -1,10 +1,8 @@
 <?php
-$localpath = getenv("SCRIPT_NAME");
-$absolutepath = getenv("SCRIPT_FILENAME");
 /** @var $_SERVER['DOCUMENT_ROOT'] /var/www/html/php-functional-programming/ */
-$_SERVER['DOCUMENT_ROOT'] = substr($absolutepath,0,strpos($absolutepath,$localpath));
-require_once $_SERVER['DOCUMENT_ROOT'].'vendor/autoload.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'src/globals.php';
+$_SERVER['DOCUMENT_ROOT'] = getcwd();
+require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/src/globals.php';
 
 spl_autoload_register(function($class) {
   $suffix   = '.php';
